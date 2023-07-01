fe407bcac0f83518815d6d4bb121bac3a04205b3
Guillermo Tala <guille86598@gmail.com>
2023-07-01 16:31:36 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {ethers} from 'ethers'
+
+import {abiContract} from './AbiContract'
+
+import {useState} from 'react'
 
 function App() {
+
+
+  const [value, setValue] = useState()
+  const [connectedAddres, setConnectedAddress] = useState()
+  const [retrievedValue, setRetrieveValue] = useState()
+
+  const {ethereum} = window;
+  const provider = new ethers.providers.Web3Provider(ethereum)
+
+    
+  const addressContract = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+  const contractInstance = new ethers.Contract(addressContract, abiContract, provider.getSigner())
+
+  const connectwalletHandler = async () => {
+    if(window.ethereum)
+      alert("Please Install Metamask!!!"); 
+
+    await provider.send("eth_requestAccounts", [])
+    const address = await provider.getSigner().getAddress();
+    setConnectedAddress(address);
+    
+  }
+
+  const store = async ()=>{
+    await contractInstance.store(value)
+  }
+
+  const retrieve = async ()=> {
+      const retrieveValue = await contractInstance.retrieve()
+      setRetrieveValue(retrieveValue.toString())
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+   
+
+        <div>
+          <button onClick={connectwalletHandler}>Connect</button>
+          <p>{connectedAddres}</p>
+        </div>
+
+        <div>
+          <button onClick={store}>store</button>
+          <input value={value} onChange={(e)=> {setValue(e.target.value)}}></input>
+        </div>
+
+        <div>
+          <button onClick={retrieve}>retrieve</button>
+          <p>{retrievedValue}</p>
+        </div>
+     
       </header>
     </div>
   );
